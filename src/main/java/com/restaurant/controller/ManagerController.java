2e0105ceb9a16605b0e8f45faf5686c37d1cd403
793490184 <793490184@qq.com>
2019-02-17 14:20:28 +0800

    manager 开发完毕 等待验证

--- src/main/java/com/restaurant/controller/ManagerController.java
+++ src/main/java/com/restaurant/controller/ManagerController.java
@@ -1,9 +1,96 @@
 package com.restaurant.controller;
 
-import org.springframework.stereotype.Component;
+import com.restaurant.entity.Account;
+import com.restaurant.entity.Expand;
+import com.restaurant.servie.ManagerService;
+import com.restaurant.utils.BaseExecution;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Component
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
 @RequestMapping("/manager")
 public class ManagerController {
+
+    @Resource
+    private ManagerService managerService;
+
+    @RequestMapping("/insert_account.do")
+    @ResponseBody
+    public BaseExecution insertAccount(String date, String name, String message, double money, int paidFlag) {
+        Account account = new Account(date, name, message, money, paidFlag);
+        managerService.insertAccount(account);
+        return new BaseExecution(200, "ok", "插入账单成功");
+    }
+
+    @RequestMapping("/delete_account.do")
+    @ResponseBody
+    public BaseExecution deleteAccount(int id) {
+        managerService.deleteAccount(id);
+        return new BaseExecution(200, "ok", "删除账单成功");
+    }
+
+    @RequestMapping("/update_account.do")
+    @ResponseBody
+    public BaseExecution updateAccount(int id, String date, String name, String message, double money, int paidFlag) {
+        Account account = new Account(id, date, name, message, money, paidFlag);
+        managerService.updateAccount(account);
+        return new BaseExecution(200, "ok", "更新账单成功");
+    }
+
+    @RequestMapping("/account_paid.do")
+    @ResponseBody
+    public BaseExecution accountPaid(int id) {
+        managerService.accountPaid(id);
+        return new BaseExecution(200, "ok", "账单支付成功");
+    }
+
+    @RequestMapping("/select_account_by_date.do")
+    @ResponseBody
+    public BaseExecution selectAccountByDate(String dateBegin, String dateEnd, int start, int end) {
+        List<Account> accountList = managerService.selectAccountByDate(dateBegin, dateEnd, start, end);
+        return new BaseExecution(200, "ok", accountList);
+    }
+
+    // expand
+    @RequestMapping("/insert_expand.do")
+    @ResponseBody
+    public BaseExecution insert(String date, String type, double money, String message) {
+        Expand expand = new Expand(date, type, money, message);
+        managerService.insert(expand);
+        return new BaseExecution(200, "ok", "插入成功");
+    }
+
+    @RequestMapping("/delete_expand.do")
+    @ResponseBody
+    public BaseExecution deleteFromDB(int id) {
+        managerService.deleteFromDB(id);
+        return new BaseExecution(200, "ok", "删除成功");
+    }
+
+    @RequestMapping("/select_expand_by_date.do")
+    @ResponseBody
+    public BaseExecution selectExpandByDate(String dateBegin, String dateEnd, int start, int end) {
+        List<Expand> expandList = managerService.selectExpandByData(dateBegin, dateEnd, start, end);
+        return new BaseExecution(200, "ok", expandList);
+    }
+
+    @RequestMapping("/select_expand_by_type.do")
+    @ResponseBody
+    public BaseExecution selectExpandByType(String type, int start, int end) {
+        List<Expand> expandList = managerService.selectExpandByType(type, start, end);
+        return new BaseExecution(200, "ok", expandList);
+    }
+
+    @RequestMapping("/select_expands.do")
+    @ResponseBody
+    public BaseExecution selectExpands(String dateBegin, String dateEnd, String type, int start, int end) {
+        List<Expand> expandList = managerService.selectExpands(dateBegin, dateEnd, type, start, end);
+        return new BaseExecution(200, "ok", expandList);
+    }
+
+
 }
--- src/main/java/com/restaurant/servie/impl/ManagerServiceImpl.java
+++ src/main/java/com/restaurant/servie/impl/ManagerServiceImpl.java
@@ -1,61 +1,74 @@
 package com.restaurant.servie.impl;
 
+import com.restaurant.dao.AccountMapper;
+import com.restaurant.dao.ExpandMapper;
 import com.restaurant.entity.Account;
 import com.restaurant.entity.Expand;
 import com.restaurant.servie.ManagerService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class ManagerServiceImpl implements ManagerService {
+
+	@Resource
+	private AccountMapper accountMapper;
+	@Resource
+	private ExpandMapper expandMapper;
+
 	@Override
 	public void insertAccount(Account account) {
-
+		accountMapper.insertAccount(account);
 	}
 
 	@Override
 	public void deleteAccount(int id) {
-
+		accountMapper.deleteAccount(id);
 	}
 
 	@Override
 	public void updateAccount(Account account) {
-
+		accountMapper.updateAccount(account);
 	}
 
 	@Override
 	public void accountPaid(int id) {
-
+		accountMapper.accountPaid(id);
 	}
 
 	@Override
 	public List<Account> selectAccountByDate(String dateBegin, String dateEnd, int start, int end) {
-		return null;
+		List<Account> accountList = accountMapper.selectAccountByDate(dateBegin, dateEnd, start, end);
+		return accountList;
 	}
 
 	@Override
 	public void insert(Expand expand) {
-
+		expandMapper.insert(expand);
 	}
 
 	@Override
 	public void deleteFromDB(int id) {
-
+		expandMapper.deleteFromDB(id);
 	}
 
 	@Override
 	public List<Expand> selectExpandByData(String dateBegin, String dateEnd, int start, int end) {
-		return null;
+		List<Expand> expandList = expandMapper.selectExpandByData(dateBegin, dateEnd, start, end);
+		return expandList;
 	}
 
 	@Override
 	public List<Expand> selectExpandByType(String type, int start, int end) {
-		return null;
+		List<Expand> expandList = expandMapper.selectExpandByType(type, start, end);
+		return expandList;
 	}
 
 	@Override
 	public List<Expand> selectExpands(String dateBegin, String dateEnd, String type, int start, int end) {
-		return null;
+		List<Expand> expandList = expandMapper.selectExpands(dateBegin, dateEnd, type, start, end);
+		return expandList;
 	}
 }
